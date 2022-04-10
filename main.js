07027b4ec423b8fc3e70ec4f31faf1d16e095162
NGO QUANG LOI <91368496+Dolly2109@users.noreply.github.com>
2022-04-10 16:00:10 +0700

    update

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-        let menu = document.querySelector('#menu-bar');
-        let navbar = document.querySelector('.navbar');
+    let menu = document.querySelector('#menu-bar');
+    let navbar = document.querySelector('.navbar');
 
-        menu.onclick = () => {
-            menu.classList.toggle('bx-x');
-            navbar.classList.toggle('active');
-        }
-        window.onscroll = () => {
-            menu.classList.remove('bx-x');
-            navbar.classList.remove('active');
-        }
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    }
+
+    alert("I wish you a good day!");
 
